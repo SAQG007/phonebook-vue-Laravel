f18d30e8185be6e1885e4d75870c3867defc7e31
SAQG007 <syedabdulqadirgillani807@gmail.com>
2022-09-09 20:23:43 +0500

    Created and implemented ContactController.php

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ContactResource;
+use App\Models\Contact;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $contacts = Contact::all();
+        return new ContactResource($contacts);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Contact::create($request->all());
+        return response(['flag' => 'saved']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $contact = Contact::find($id);
+        return new ContactResource($contact);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $contact = Contact::find($id);
+        $contact->update($request->all());
+//        return response(['flag' => 'updated']);
+        return new ContactResource($contact);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Contact::find($id)->delete();
+//        $contact->delete();
+
+        return response(['flag' => 'deleted']);
+    }
+}
