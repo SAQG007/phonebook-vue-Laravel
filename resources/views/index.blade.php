fd7369b64993d71aaa3cfe7f6cb739bdd6722c12
SAQG007 <syedabdulqadirgillani807@gmail.com>
2022-09-10 19:36:15 +0500

    Designed index using contact vue

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,16 +3,29 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Phonebook</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+        <link crossorigin="anonymous" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css"
+              integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT"
+              rel="stylesheet">
+        <script>
+            window.baseUrl = "{{ config('app.url') }}:8000/api/";
+        </script>
     </head>
 
     <body>
         <div id="app">
-            <example-component></example-component>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col">
+                        <contact></contact>
+                    </div>
+                </div>
+            </div>
         </div>
 
-        <script src="{{ mix('js/app.js') }}"></script>
+        <script src="{{ mix('/js/app.js') }}"></script>
+        <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
     </body>
 </html>
